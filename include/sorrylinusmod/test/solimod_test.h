5e8a13338256ea55528d64e06284f5b7133e8df8
seantywork <seantywork@gmail.com>
2023-09-12 12:02:06 +0000

    solitestmod - uname

--- include/sorrylinusmod/test/solimod_test.h
+++ include/sorrylinusmod/test/solimod_test.h
@@ -1,8 +1,10 @@
 #ifndef SOLI_HEADER_TEST
 #define SOLI_HEADER_TEST
 
+#include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include <sys/utsname.h>
-char* soli_test_method();
+char* soli_test_uname();
 
 #endif
--- lib/sorrylinusmod/test/solimod_test.c
+++ lib/sorrylinusmod/test/solimod_test.c
@@ -1,21 +1,33 @@
+
 #include <sorrylinusmod/test/solimod_test.h>
 
 
 
+char* soli_test_uname(){
 
-char* soli_test_method(){
 
-    char* str_ptr;
     struct utsname uname_ptr;
     uname(&uname_ptr);
 
-    printf("system name - %s \n", uname_ptr.sysname);
-    printf("node name   - %s \n", uname_ptr.nodename);
-    printf("release     - %s \n", uname_ptr.release);
-    printf("version     - %s \n", uname_ptr.version);
-    printf("machine     - %s \n", uname_ptr.machine);
-    
+    char* str_ptr = (char*)malloc(500);
+    strcat(str_ptr,"system name - ");
+    strcat(str_ptr,uname_ptr.sysname);
+    strcat(str_ptr,"\n");
+    strcat(str_ptr,"node name   - ");
+    strcat(str_ptr,uname_ptr.nodename);
+    strcat(str_ptr,"\n");
+    strcat(str_ptr,"release     - ");
+    strcat(str_ptr,uname_ptr.release);
+    strcat(str_ptr,"\n");
+    strcat(str_ptr,"version     - ");
+    strcat(str_ptr,uname_ptr.version);
+    strcat(str_ptr,"\n");
+    strcat(str_ptr,"machine     - ");
+    strcat(str_ptr,uname_ptr.machine);
+    strcat(str_ptr,"\n"); 
 
-    return 0;  
+
+    return str_ptr;  
 
 }
+
