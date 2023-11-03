7cbb070c7df2e3b7c5a5f38f0a6b8a247cdb6f2e
seantywork <seantywork@gmail.com>
2023-11-03 18:39:17 +0900

    firewall rules

--- hub/install.sh
+++ hub/install.sh
@@ -61,3 +61,15 @@ sudo snap install --classic certbot
 
 sudo ln -s /snap/bin/certbot /usr/bin/certbot 
 
+# firewall
+
+
+# sudo ufw allow 22
+
+# sudo ufw allow 80
+
+# sudo ufw allow 443
+
+# sudo ufw allow 3000
+
+# sudo ufw allow 3001
