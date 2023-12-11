3a56a891b8a6d0296deea5613805655eae5d7363
seantywork <seantywork@gmail.com>
2023-12-11 13:57:06 +0900

    ctrl-worker k8s mode set, installation process, nginx hybrid

--- hub/install.sh
+++ hub/install.sh
@@ -25,9 +25,9 @@ sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-
 sudo chmod +x /usr/local/bin/docker-compose
 
 
-# kind
+# #kind
 
-[ $(uname -m) = x86_64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.20.0/kind-linux-amd64
+# #[ $(uname -m) = x86_64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.20.0/kind-linux-amd64
 
 
 chmod +x ./kind
@@ -39,21 +39,6 @@ sudo mv ./kind /usr/local/bin/kind
 sudo apt update
 sudo apt install nginx
 
-
-# kubectl
-
-sudo apt-get update
-
-sudo apt-get install -y apt-transport-https ca-certificates curl
-
-curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.27/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
-
-
-echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.27/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
-
-sudo apt-get update
-
-sudo apt-get install -y kubectl
 
 # certbot
 
