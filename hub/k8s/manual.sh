9c5db511b520bd6f93ccab4db9cf380ac8feb53b
seantywork <seantywork@gmail.com>
2023-12-11 11:39:03 +0900

    ingres

--- hub/k8s/manual.sh
+++ hub/k8s/manual.sh
@@ -5,8 +5,6 @@ kubectl create namespace frank
 
 kubectl create secret generic docker-secret --from-file=.dockerconfigjson=/root/.docker/config.json --type=kubernetes.io/dockerconfigjson -n frank
 
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
-
 kubectl -n frank apply -f ./app.yaml
 
 kubectl -n frank apply -f ./ingress.yaml
