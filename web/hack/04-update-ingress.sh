cfa0b588cb54d31123d37a945d38d81508f1899c
seantywork <seantywork@gmail.com>
2024-07-21 17:56:24 +0900

    fix bin/cp

--- web/hack/04-update-ingress.sh
+++ web/hack/04-update-ingress.sh
@@ -3,9 +3,9 @@
 
 
 
-bin/cp -Rf ../nginx/fd.conf /etc/nginx/conf.d/fd.conf 
+/bin/cp -Rf ../nginx/fd.conf /etc/nginx/conf.d/fd.conf 
 
-bin/cp -Rf ../nginx/nginx.conf /etc/nginx/nginx.conf
+/bin/cp -Rf ../nginx/nginx.conf /etc/nginx/nginx.conf
 
 systemctl restart nginx
 
